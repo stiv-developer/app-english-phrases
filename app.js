4c13a8f1045f653cd5fca2bed2988f51b8aff981
stiv-developer <estvrak100@gmail.com>
2024-10-01 22:56:56 -0500

    Se modifico el menu

--- app.js
+++ app.js
@@ -1,91 +1,92 @@
 document.addEventListener('DOMContentLoaded', function () {
     const accordion = document.getElementById('accordionPanelsStayOpenExample');
+    const languageSelect = document.getElementById('language-select');
+    const selectedLanguage = localStorage.getItem('selectedLanguage') || 'translation_spanish';
 
-    // Verificar si hay un idioma guardado en localStorage (por defecto será español)
-    let selectedLanguage = localStorage.getItem('selectedLanguage') || 'translation_spanish';
-
-    // Función para actualizar el contenido en el idioma seleccionado
-    function updateContent(jsonData, language) {
-        accordion.innerHTML = ''; // Limpiar contenido previo
-        jsonData['title-phrases'].forEach((item, index) => {
-            const itemId = `panelsStayOpen-collapse${index}`;
-
-            let accordionItemHTML = `
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed fw-semibold" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#${itemId}" aria-expanded="false" aria-controls="${itemId}">
-                            ${item.phrases} (${item['phrases-translation']})
-                        </button>
-                    </h2>
-                    <div id="${itemId}" class="accordion-collapse collapse" aria-labelledby="heading${index}">
-                        <div class="accordion-body">
-                            <!-- Phrases will be injected here -->
-                        </div>
-                    </div>
-                </div>
-            `;
-
-            // Agregar el item del acordeón
-            accordion.innerHTML += accordionItemHTML;
-            const bodyContainer = document.querySelector(`#${itemId} .accordion-body`);
-
-            // Si existe 'type', recorrer sus elementos
-            if (item.type) {
-                item.type.forEach(typeItem => {
-                    const typeContainer = document.createElement('div');
-                    if (typeItem['color-card']) {
-                        typeContainer.classList.add(typeItem['color-card']);
-                    }
-                    typeContainer.classList.add('group-type', typeItem['name-type'].toLowerCase().replace(/\s+/g, '-'));
-
-                    const nameTypeTranslation = typeItem['name-type-translation'] ? `<span class="fs-6 text-success"> (${typeItem['name-type-translation']})</span>` : '';
-                    const typeHeaderHTML = `<h5>${typeItem['name-type']}${nameTypeTranslation}</h5>`;
-                    typeContainer.innerHTML += typeHeaderHTML;
-
-                    typeItem.phrases.forEach(phraseItem => {
-                        const phraseHTML = `
-                            <div class="group-phrase">
-                                <div class="phrase">
-                                    <i class="bi bi-circle-fill"></i>
-                                    <p class="p-phrase fw-semibold">${phraseItem.phrase}</p>
-                                </div>
-                                <p class="p-phrase-translate text-secondary-emphasis">${phraseItem[language]}</p>
-                            </div>
-                        `;
-                        typeContainer.innerHTML += phraseHTML;
-                    });
-
-                    bodyContainer.appendChild(typeContainer);
-                });
-            }
-        });
+    // Aplicar idioma guardado al cargar la página
+    if (selectedLanguage === 'translation_portuguese') {
+        languageSelect.value = 'portuguese';
+    } else {
+        languageSelect.value = 'spanish';
     }
 
-    // Fetch del archivo JSON
-    fetch('data.json')
-        .then(response => response.json())
-        .then(jsonData => {
-            // Actualizar el contenido con el idioma seleccionado
-            updateContent(jsonData, selectedLanguage);
+    // Función para aplicar las traducciones
+    function applyTranslations(language) {
+        fetch('data.json')
+            .then(response => response.json())
+            .then(jsonData => {
+                // Limpiar el contenido existente
+                accordion.innerHTML = '';
 
-            // Event listener para cambios de idioma desde la página de configuración
-            window.addEventListener('storage', (event) => {
-                if (event.key === 'selectedLanguage') {
-                    selectedLanguage = event.newValue;
-                    updateContent(jsonData, selectedLanguage);
-                }
-            });
-        })
-        .catch(error => console.error('Error loading JSON data:', error));
+                jsonData['title-phrases'].forEach((item, index) => {
+                    const itemId = `panelsStayOpen-collapse${index}`;
 
-    // MENU BAR 
-    $(document).ready(function(){
-        $('.toggle').click(function(){
-            $('.menu').toggleClass('active');
-        });
+                    let accordionItemHTML = `
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed fw-semibold" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#${itemId}" aria-expanded="false" aria-controls="${itemId}">
+                                    ${item.phrases} (${item['phrases-translation']})
+                                </button>
+                            </h2>
+                            <div id="${itemId}" class="accordion-collapse collapse" aria-labelledby="heading${index}">
+                                <div class="accordion-body"></div>
+                            </div>
+                        </div>
+                    `;
+
+                    accordion.innerHTML += accordionItemHTML;
+                    const bodyContainer = document.querySelector(`#${itemId} .accordion-body`);
+
+                    if (item.type) {
+                        item.type.forEach(typeItem => {
+                            const typeContainer = document.createElement('div');
+                            typeContainer.classList.add('group-type', typeItem['name-type'].toLowerCase().replace(/\s+/g, '-'));
+
+                            const nameTypeTranslation = typeItem['name-type-translation'] ? `<span class="fs-6 text-success"> (${typeItem['name-type-translation']})</span>` : '';
+                            const typeHeaderHTML = `<h5>${typeItem['name-type']}${nameTypeTranslation}</h5>`;
+
+                            typeContainer.innerHTML += typeHeaderHTML;
+
+                            typeItem.phrases.forEach(phraseItem => {
+                                const phraseHTML = `
+                                    <div class="group-phrase">
+                                        <div class="phrase">
+                                            <i class="bi bi-circle-fill"></i>
+                                            <p class="p-phrase fw-semibold">${phraseItem.phrase}</p>
+                                        </div>
+                                        <p class="p-phrase-translate text-secondary-emphasis">${phraseItem[language]}</p>
+                                    </div>
+                                `;
+                                typeContainer.innerHTML += phraseHTML;
+                            });
+
+                            bodyContainer.appendChild(typeContainer);
+                        });
+                    }
+                });
+            })
+            .catch(error => console.error('Error loading JSON data:', error));
+    }
+
+    // Aplicar las traducciones al cargar la página
+    applyTranslations(selectedLanguage);
+
+    // Escuchar cambios en el select de idioma
+    languageSelect.addEventListener('change', function () {
+        const selectedOption = this.value;
+        const languageKey = selectedOption === 'spanish' ? 'translation_spanish' : 'translation_portuguese';
+        localStorage.setItem('selectedLanguage', languageKey);
+        applyTranslations(languageKey);  // Aplicar traducción inmediatamente
+    });
+
+    // Si se detecta un cambio en localStorage en otras pestañas
+    window.addEventListener('storage', function () {
+        const newLanguage = localStorage.getItem('selectedLanguage');
+        applyTranslations(newLanguage);
     });
 });
+
 
 // En setting.html
 document.addEventListener('DOMContentLoaded', function () {
@@ -112,5 +113,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Disparar evento 'storage' para actualizar otras páginas que estén usando el idioma seleccionado
         window.dispatchEvent(new Event('storage'));
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Obtener el elemento del select
+    const themeSelect = document.getElementById('theme-select');
+
+    //Obtener tema almacenado en localStorage si exite
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.body.classList.toggle('dark-mode', savedTheme === 'dark');
+        themeSelect.value = savedTheme;
+    }
+
+    // Cambiar el tema segun el valodr del select
+    themeSelect.addEventListener('change', function () {
+        const selectedTheme = themeSelect.value;
+        if (selectedTheme === 'dark') {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark'); // Almacenar tema en localStorage
+        } else if (selectedTheme === 'light') {
+            document.body.classList.remove('dark-mode');
+            this.localName.setItem('theme', 'light');
+        }
+    });
+
+})
+
+// MENU BAR 
+$(document).ready(function () {
+    $('.toggle').click(function () {
+        $('.menu').toggleClass('active');
     });
 });
